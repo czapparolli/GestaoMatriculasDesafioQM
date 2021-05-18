630f0d8a2294ea61d8fee32ce966ac8c74ea907d
czapparolli <caio_zapparolli@hotmail.com>
2021-05-18 12:47:13 -0300

    18/05/2021/

--- ProgramaPrincipal.java
+++ ProgramaPrincipal.java
@@ -3,6 +3,18 @@ import java.util.Scanner;
 
 public class ProgramaPrincipal {
 
+	public static void pausa() {
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException ex) {
+		}
+	}
+
+	public static void cls() {
+		for (int i = 0; i < 50; i++)
+			System.out.println("");
+	}
+
 	public static void menuCadastrosPrincipal(Scanner teclado) {
 
 		int opcaoMenuCadastroPrincipal = 0;
@@ -20,6 +32,7 @@ public class ProgramaPrincipal {
 			case 1:
 				menuCadastroAlunos(teclado);
 				break;
+			case 2: menuCadastroProfessores(teclado);
 
 			default:
 				System.out.println("Opção inválida!");
@@ -28,36 +41,98 @@ public class ProgramaPrincipal {
 		} while (opcaoMenuCadastroPrincipal != 0);
 
 	}
-
+	
 	public static void menuCadastroAlunos(Scanner teclado) {
 
 		int opcaoMenuCadastroPrincipal = 0;
 
 		do {
-			System.out.println("\n------------------- MENU CADASTRO ALUNOS -------------------\n\n");
+			System.out.println("\n------------------- MENU CADASTRO ALUNOS -------------------\n");
 			System.out.println("[1] - Cadastrar");
 			System.out.println("[2] - Alterar");
 			System.out.println("[3] - Excluir");
 			System.out.println("[4] - Consultar");
 			System.out.println("[5] - Listar Todos");
-			System.out.println("[6] - menu anterior");
+			System.out.println("[6] - Menu anterior");
 			System.out.print("\nDigite a opção: ");
 			opcaoMenuCadastroPrincipal = teclado.nextInt();
 
 			switch (opcaoMenuCadastroPrincipal) {
 			case 1:
 				cadastraSqlAluno(teclado);
+				pausa();
 				break;
+			case 2:
+				alteraSqlAluno(teclado);
+				pausa();
+				break;
+			case 3:
+				excluiSqlAluno(teclado);
+				pausa();
+				break;
+			case 4:
+				consultaSqlAluno(teclado);
+				pausa();
+				break;
+			case 5:
+				listaTodosSqlAluno();
+				pausa();
+				break;
+			default:
+				System.out.println("Opção inválida!");
+				break;
+			}
+		} while (opcaoMenuCadastroPrincipal != 6);
+		System.out.println("\nRetornando ao menu anterior...");
 
+	}
+
+	public static void menuCadastroProfessores(Scanner teclado) {
+
+		int opcaoMenuCadastroPrincipal = 0;
+
+		do {
+			System.out.println("\n------------------- MENU CADASTRO PROFESSORES -------------------\n");
+			System.out.println("[1] - Cadastrar");
+			System.out.println("[2] - Alterar");
+			System.out.println("[3] - Excluir");
+			System.out.println("[4] - Consultar");
+			System.out.println("[5] - Listar Todos");
+			System.out.println("[6] - Menu anterior");
+			System.out.print("\nDigite a opção: ");
+			opcaoMenuCadastroPrincipal = teclado.nextInt();
+
+			switch (opcaoMenuCadastroPrincipal) {
+			case 1:
+				cadastraSqlAluno(teclado);
+				pausa();
+				break;
+			case 2:
+				alteraSqlAluno(teclado);
+				pausa();
+				break;
+			case 3:
+				excluiSqlAluno(teclado);
+				pausa();
+				break;
+			case 4:
+				consultaSqlAluno(teclado);
+				pausa();
+				break;
+			case 5:
+				listaTodosSqlAluno();
+				pausa();
+				break;
 			default:
 				System.out.println("Opção inválida!");
 
 			}
 		} while (opcaoMenuCadastroPrincipal != 6);
+		System.out.println("\nRetornando ao menu anterior...");
 
 	}
-
-	public static boolean cadastraSqlAluno(Scanner teclado) {
+	
+	public static void cadastraSqlAluno(Scanner teclado) {
 
 		try {
 
@@ -69,90 +144,122 @@ public class ProgramaPrincipal {
 			String nomeAluno = teclado.nextLine();
 			System.out.print("\nDigite o CPF do Aluno: ");
 			String cpf = teclado.nextLine();
-			String sqlCadastro = "INSERT into aluno (nomeAluno, cpf)VALUES" + "('"+ nomeAluno + "','" + cpf + "')";
-			
-		//	String sqlCadastro = "INSERT into aluno (codigoAluno, nomeAluno, cpf)VALUES" + "('" + codigoAluno + "','"
-				//	+ nomeAluno + "','" + cpf + "')";
+			String sqlCadastro = "INSERT into aluno (nomeAluno, cpf)VALUES" + "('" + nomeAluno + "','" + cpf + "')";
+
 			Statement stmCadastrado = con.createStatement();
 			int res = stmCadastrado.executeUpdate(sqlCadastro);
 
 			if (con != null) {
 				System.out.println("\nCadastrado com sucesso");
 				con.close();
-				return true;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return false;
 
 	}
 
-	public static boolean consultaSqlAluno() {
+	public static void alteraSqlAluno(Scanner teclado) {
 		try {
 			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/GestaoDisciplinas",
 					"postgres", "Panda104455!");
-			String sqlConsulta = "SELECT * FROM ALUNO";
+			System.out.println("\nAtualizando DADOS");
+			System.out.print("\nDigite o código do aluno: ");
+			int codigoAluno = teclado.nextInt();
+			teclado.nextLine();
+			System.out.print("\nDigite a alteração do nome: ");
+			String alteraNome = teclado.nextLine();
+			String sqlAltera = "update aluno set nomealuno = '" + alteraNome + "' where codigoAluno=" + codigoAluno;
+			Statement stmConsulta = con.createStatement();
+			stmConsulta.executeUpdate(sqlAltera);
+			System.out.println("Dados atualizados na tabela");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void excluiSqlAluno(Scanner teclado) {
+
+		try {
+			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/GestaoDisciplinas",
+					"postgres", "Panda104455!");
+			System.out.println("\nTELA DE EXCLUSÃO DE ALUNOS");
+			System.out.print("\nDigite o código do aluno para exclusão: ");
+			int codigoAluno = teclado.nextInt();
+			String sqlExclui = "DELETE FROM aluno where codigoAluno = " + codigoAluno;
+			Statement stmConsulta = con.createStatement();
+			stmConsulta.executeUpdate(sqlExclui);
+
+			System.out.println("\nAtualizando DADOS...");
+			pausa();
+			System.out.println("\nAluno excluido da tabela com sucesso !");
+			pausa();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void consultaSqlAluno(Scanner teclado) {
+
+		try {
+			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/GestaoDisciplinas",
+					"postgres", "Panda104455!");
+
+			System.out.println("\nMENU DE CONSULTAS DE ALUNO INDIVIDUAL");
+			teclado.nextLine();
+			System.out.print("\nDigite o nome completo do aluno:  ");
+			String nomeAluno = teclado.nextLine();
+			String sqlConsulta = "SELECT * FROM ALUNO where nomealuno like '" + nomeAluno + "'\n";
 			Statement stmConsulta = con.createStatement();
 			ResultSet result = null;
 			result = stmConsulta.executeQuery(sqlConsulta);
 
+			System.out.println("\n--------------- CONSULTANDO DADOS DOS ALUNOS ---------------\n");
 			while (result.next()) {
+				System.out.printf("Codigo: %d, Nome: %s, CPF: %s", result.getInt(1), result.getString(2),
+						result.getString(3));
+				System.out.println();
 
-				System.out.println("CONSULTANDO DADOS");
-				System.out.println("Codigo aluno....: " + result.getInt(1));
-				System.out.println("Nome aluno....: " + result.getString(2));
-				System.out.println("CPF aluno....: " + result.getString(3));
-				return true;
 			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return false;
 
 	}
 
-	public static boolean alteraSqlAluno() {
+	public static void listaTodosSqlAluno() {
 		try {
 			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/GestaoDisciplinas",
 					"postgres", "Panda104455!");
-			String sqlAltera = "UPDATE ALUNO SET cpf = 35555578369";
+			String sqlConsulta = "SELECT * FROM ALUNO ORDER BY codigoAluno";
 			Statement stmConsulta = con.createStatement();
-			stmConsulta.executeUpdate(sqlAltera);
+			ResultSet result = null;
+			result = stmConsulta.executeQuery(sqlConsulta);
+			// cls();
+			System.out.println("\n--------------- CONSULTANDO DADOS DOS ALUNOS ---------------\n");
 
-			System.out.println("Atualizando DADOS");
-			System.out.println("Dados atualizados na tabela");
-			return true;
+			while (result.next()) {
+				// if (result == null) {
+				// System.out.println("Não temos alunos cadastrados no momento :(");
+				// }
+				System.out.printf("\nCodigo: %d, Nome: %s, CPF: %s", result.getInt(1), result.getString(2),
+						result.getString(3));
+				System.out.println();
+
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return false;
-	}
 
-	public static boolean excluiSqlAluno() {
-
-		try {
-			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/GestaoDisciplinas",
-					"postgres", "Panda104455!");
-			String sqlExclui = "DELETE FROM aluno";
-			Statement stmConsulta = con.createStatement();
-			stmConsulta.executeUpdate(sqlExclui);
-
-			System.out.println("Atualizando DADOS");
-			System.out.println("Dados excluidos da tabela");
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
 	}
 
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
-		String url = "jdbc:postgresql://localhost:5432/GestaoDisciplinas";
-		String usuario = "postgres";
-		String senha = "Panda104455!";
+		//String url = "jdbc:postgresql://localhost:5432/GestaoDisciplinas";
+		//String usuario = "postgres";
+		//String senha = "Panda104455!";
 		ResultSet result = null;
 
 		int opcaoMenuPrincipal = 0;
@@ -182,8 +289,5 @@ public class ProgramaPrincipal {
 
 		} while (opcaoMenuPrincipal != 0);
 
-		// consultando
-
-		// System.out.printf("os dados foram\n", consultaSql());
 	}
 }
